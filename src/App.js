f9bd9609e66bc642093d2f361bf9c4d4fafd7cb6
IvySwing <entense@att.net>
2022-12-07 13:15:49 -0500

    update css & add conditional to execute alert

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./styles.css";
 
 import Game from "../src/components/Game";
+import { useGameStats } from "./hooks/useGameStats";
 
 export default function App() {
+  const [gameStats, addLinesCleared] = useGameStats();
+  console.log(gameStats.linesCompleted);
   return (
     <div className="App">
-      <Game rows={20} columns={10} />
+      {gameStats.level >= 3 ? (
+        <p>passcode is "whatever"</p>
+      ) : (
+        <Game rows={20} columns={10} />
+      )}
     </div>
   );
 }
