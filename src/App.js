31d9072628bc436f61603254904eab00a6a13014
IvySwing <entense@att.net>
2022-12-09 14:22:07 -0500

    update css

--- src/App.js
+++ src/App.js
@@ -8,11 +8,7 @@ export default function App() {
   console.log(gameStats.linesCompleted);
   return (
     <div className="App">
-      {gameStats.level >= 3 ? (
-        <p>passcode is "whatever"</p>
-      ) : (
-        <Game rows={20} columns={10} />
-      )}
+      <Game rows={20} columns={10} />
     </div>
   );
 }
